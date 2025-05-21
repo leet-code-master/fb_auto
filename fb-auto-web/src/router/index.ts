93af81b903db870c1864efe34b254325e203a427
coder <1846475817@qq.com>
2025-05-21 22:00:30 +0800

    init

---
+++ fb-auto-web/src/router/index.ts
@@ -0,0 +1,23 @@
+import {
+  createRouter,
+  createWebHistory,
+  type RouteRecordRaw,
+} from "vue-router";
+
+const routes: Array<RouteRecordRaw> = [
+  {
+    path: "/",
+    component: () => import("@/views/home/index.vue"),
+  },
+];
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+});
+
+router.beforeEach((to, from, next) => {
+  next();
+});
+
+export default router;
